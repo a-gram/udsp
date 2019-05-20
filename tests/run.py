bcf6b602bb57d64cef3d381a6fc7712b22db6fdd
Albert <alb.gramaglia@audioneex.com>
2019-05-21 03:16:24 +1000

    Fixed loader

--- tests/run.py
+++ tests/run.py
@@ -1,7 +1,7 @@
 import unittest
 
-
+# Run from the root dir with >python ./tests/run.py
 if __name__ == "__main__":
-    tests = unittest.defaultTestLoader.discover(".")
+    tests = unittest.defaultTestLoader.discover("./tests")
     runner = unittest.TextTestRunner()
     runner.run(tests)
